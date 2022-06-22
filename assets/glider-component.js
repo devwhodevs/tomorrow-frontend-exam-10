333561679aad534b40102b06cceea4111be77de1
Oleksandr Ostrovskyi <devwhodevs@gmail.com>
2022-06-22 15:53:50 +0300

    feat: Header, mobile menu, animation created

---
+++ assets/glider-component.js
@@ -0,0 +1,12 @@
+console.log('glider-component.js')
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('glider', (params = { slidesToShow: 1 }) => ({
+        gliderInstance: null,
+        init() {
+            this.$nextTick(() => {
+                this.gliderInstance = new Glider(this.$el, {...params})
+            })
+        }
+    }))
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./**/*.{html,js,liquid}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'platform': ['Platform', 'sans-serif'],
+      'gt-super-text': ['GT Super Text', 'serif'],
+      'gt-super-display': ['GT Super Display', 'serif']
+    },
+    extend: {
+      colors: {
+        yellow: '#E3FF21',
+        blue: '#85ABBD',
+        green: '#00966E',
+        cream: '#F4EDE5'
+      },
+      screens: {
+        '2xl': '1440px'
+      }
+    },
   },
   plugins: [],
 }
